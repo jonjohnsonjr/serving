29b7429a050334ee0f08e4241eba577a68722b12
Jon Johnson <jonjohnson@google.com>
2019-06-25 10:01:24 -0700

    noclient

--- cmd/resolver/main.go
+++ cmd/resolver/main.go
@@ -4,42 +4,27 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/google/go-containerregistry/pkg/authn/k8schain"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/v1/remote"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	var (
-		masterURL      = flag.String("master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
-		kubeconfig     = flag.String("kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
-		namespace      = "default"
-		serviceAccount = "default"
-		image          = "gcr.io/jonjohnson-test/crane"
-	)
 	flag.Parse()
 
-	// image := os.Args[1]
+	image := os.Getenv("IMAGE")
+	if image == "" {
+		image = "ubuntu"
+	}
+
 	tag, err := name.NewTag(image, name.WeakValidation)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	opt := k8schain.Options{
-		Namespace:          namespace,
-		ServiceAccountName: serviceAccount,
-	}
-
-	cfg, err := clientcmd.BuildConfigFromFlags(*masterURL, *kubeconfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-	client := kubernetes.NewForConfigOrDie(cfg)
-
-	kc, err := k8schain.New(client, opt)
+	kc, err := k8schain.NewNoClient()
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -49,5 +34,5 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(desc.Digest)
+	fmt.Printf("%s = %s", image, desc.Digest)
 }
